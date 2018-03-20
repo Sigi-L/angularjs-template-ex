a98836413f47a15bb06b6a25c86623c0b3863b3a
sigi-l <sigi.lahiany@gmail.com>
2018-03-20 11:06:58 +0200

    Add field selected to actor constractor

--- exCtrl.js
+++ exCtrl.js
@@ -1,9 +1,10 @@
 myApp.controller('exCtrl', ['$scope', function ($scope) {
     $scope.msg = 'This Must Work!';
 
-    function Actor(fname, lname, photo, imdbLink, bDate) {
+    function Actor(fname, lname, selected, photo, imdbLink, bDate) {
         this.fname = fname;
         this.lname = lname;
+        this.selected = false;
         this.photo = photo;
         this.imdbLink = imdbLink;
         this.bDate = new Date(bDate);
@@ -12,32 +13,32 @@ myApp.controller('exCtrl', ['$scope', function ($scope) {
         }
     }
 
-    var actor1 = new Actor("Jack", "Nicholson",
+    var actor1 = new Actor("Jack", "Nicholson", false,
         "https://ia.media-imdb.com/images/M/MV5BMTQ3OTY0ODk0M15BMl5BanBnXkFtZTYwNzE4Njc4._V1_UY317_CR7,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000197/?ref_=nmls_pst",
         "04-22-1937");
     //  alert(JSON.stringify(actor1));
 
-    var actor2 = new Actor("Dustin", "Hoffman",
+    var actor2 = new Actor("Dustin", "Hoffman", false,
         "https://ia.media-imdb.com/images/M/MV5BMTc3NzU0ODczMF5BMl5BanBnXkFtZTcwODEyMDY5Mg@@._V1_UY209_CR8,0,140,209_AL_.jpg",
         "http://www.imdb.com/name/nm0000163/?ref_=nmls_hd",
         "08-08-1937");
 
-    var actor3 = new Actor("Paul", "Newman",
+    var actor3 = new Actor("Paul", "Newman",false,
         "https://ia.media-imdb.com/images/M/MV5BODUwMDYwNDg3N15BMl5BanBnXkFtZTcwODEzNTgxMw@@._V1_UY317_CR22,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000056/?ref_=nmls_hd",
         "01-26-1925");
 
-    var actor4 = new Actor("Charles", "Chaplin",
+    var actor4 = new Actor("Charles", "Chaplin",false,
         "https://ia.media-imdb.com/images/M/MV5BNDcwMDc0ODAzOF5BMl5BanBnXkFtZTgwNTY2OTI1MDE@._V1_UX214_CR0,0,214,317_AL_.jpg",
         "http://www.imdb.com/name/nm0000122/?ref_=nmls_hd",
         "12-25-1889");
-    var actor5 = new Actor("Johnny", "Depp",
+    var actor5 = new Actor("Johnny", "Depp", false,
         "https://ia.media-imdb.com/images/M/MV5BMTM0ODU5Nzk2OV5BMl5BanBnXkFtZTcwMzI2ODgyNQ@@._V1_UY209_CR3,0,140,209_AL_.jpg",
         "http://www.imdb.com/name/nm0000136/?ref_=nmls_hd",
         "06-09-1963");
 
-    var actor6 = new Actor("Robert", "Redford",
+    var actor6 = new Actor("Robert", "Redford",false,
         "https://ia.media-imdb.com/images/M/MV5BMTk1Nzc5MzQyMV5BMl5BanBnXkFtZTcwNjQ5OTA0Mg@@._V1_UY209_CR5,0,140,209_AL_.jpg",
         "http://www.imdb.com/name/nm0000602/?ref_=nmls_hd",
         "08-18-1936");
@@ -71,7 +72,7 @@ myApp.controller('exCtrl', ['$scope', function ($scope) {
 
 
     $scope.addActor = function () {
-        var newActorObj = new Actor($scope.newActor.fname, $scope.newActor.lname,
+        var newActorObj = new Actor($scope.newActor.fname, $scope.newActor.lname, false,
             $scope.newActor.photo, $scope.newActor.imdbLink, new Date($scope.newActor.bDate));
 
         $scope.actors.push(newActorObj);
